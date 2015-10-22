8d5313fbb9a5e63481f5ce407464d09072bea2e9
David\David Crotty <david.crotty.work@gmail.com>
2015-10-22 21:41:57 +0100

    Update ignore. Working static content nginx config. Box will spin up web server on boot.

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -1,4 +1,13 @@
+#Install software
 sudo apt-get update
 sudo apt-get -y install nginx
+
+#Assign permissions to nginx user, make web dir public
 sudo chown -R www-data:www-data /var/www/sites/house_LED
 sudo chmod 755 /var/www
+
+#Add Vhost entries and enable site
+cp /vagrant/config/house-led.dev /etc/nginx/sites-available/house-led.dev
+rm /etc/nginx/sites-available/default
+sudo ln -s /etc/nginx/sites-available/house-led.dev /etc/nginx/sites-enabled/house-led.dev
+sudo service nginx restart
