41e1517eeadd78351a18e5bbdca0d1ae656b8d52
David Crotty <david.crotty.work@gmail.com>
2015-12-19 14:17:27 +0000

    setup software enviroment.

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -3,7 +3,16 @@ sudo apt-get update
 sudo apt-get -y install nginx
 sudo apt-get -y install python-pip
 sudo apt-get -y install python-virtualenv
+sudo apt-get -y install python-setuptools
 sudo apt-get -y install python-dev
+
+#Setup software enviroment
+sudo virtualenv /var/www/sites/house-led.dev/house-led
+source /var/www/sites/house-led.dev/house-led/bin/activate
+cd /var/www/sites/house-led.dev/house-led
+pip install Django
+django-admin.py startproject house_led
+cd house_led
 
 #Assign permissions to nginx user, make web dir public
 sudo chown -R www-data:www-data /var/www/sites/house-led.dev
